fc614d292e71a527dad9f773cd339f6dc06601cd
Cesare de Cal <contact@cesare.io>
2018-03-26 15:32:00 +0200

    updated code

--- 26-03/Semaforo.java
+++ 26-03/Semaforo.java
@@ -1,5 +1,24 @@
 public class Semaforo {
-  public int colore;
+  private int colore;
+
+  public int getColore() {
+    return colore;
+  }
+
+  // setter
+  private void setColore(int colore) {
+    if (colore == 0 || colore == 1 | colore == 2) {
+      this.colore = colore;
+    }
+  }
+
+  public void cambiaColore() {
+    if (colore == 2) {
+      setColore(0);
+    } else {
+      setColore(colore++);
+    }
+  }
 
   public String toString() {
     String stato = null;
@@ -14,5 +33,27 @@ public class Semaforo {
       }
     }
     return stato;
+  }
+
+  public Semaforo() {
+    setColore(0);
+  }
+
+  public Semaforo(int colore) {
+    setColore(colore);
+  }
+
+  public boolean equals(Semaforo altro) {
+    if (altro == null) {
+      return false;
+    }
+
+    if (this == altro) {
+      return true;
+    }
+
+    if (colore == altro.colore) {
+      return true;
+    }
   }
 }
--- 26-03/TestSemaforo.java
+++ 26-03/TestSemaforo.java
@@ -1,8 +1,7 @@
 public class TestSemaforo {
   public static void main(String a[]) {
-    Semaforo s1 = new Semaforo();
-    s1.colore = 0;
-    System.out.println(s1);
+    Semaforo s1 = new Semaforo(0);
+    System.out.println(s1.getColore());
     System.out.println("ciao");
   }
 }
