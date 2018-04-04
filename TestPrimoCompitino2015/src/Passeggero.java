bf4e1625ac06e39d64db959940e355f69f454082
Cesare de Cal <contact@cesare.io>
2018-04-04 10:26:04 +0200

    Finishing up Java project

--- TestPrimoCompitino2015/src/Passeggero.java
+++ TestPrimoCompitino2015/src/Passeggero.java
@@ -3,11 +3,35 @@ public class Passeggero {
 	private boolean inPossessoDiBiglietto;
 		
 	public Passeggero(String destinazione) {
-		this.destinazione = destinazione;
+		setAttributes(destinazione, true);
 	}
 
 	public Passeggero(String destinazione, boolean inPossessoDiBiglietto) {
+		setAttributes(destinazione, inPossessoDiBiglietto);
+	}
+	
+	private void setAttributes(String destinazione, boolean inPossessoDiBiglietto) {
+		if (destinazione == null) {
+			this.destinazione = "CAPOLINEA";
+		} else {
+			this.destinazione = destinazione;
+		}
+		this.inPossessoDiBiglietto = inPossessoDiBiglietto;
+	}
+	
+	public String getDestinazione() {
+		return destinazione;
+	}
+
+	public void setDestinazione(String destinazione) {
 		this.destinazione = destinazione;
+	}
+
+	public boolean isInPosessoDiBiglietto() {
+		return inPossessoDiBiglietto;
+	}
+
+	public void setInPossessoDiBiglietto(boolean inPossessoDiBiglietto) {
 		this.inPossessoDiBiglietto = inPossessoDiBiglietto;
 	}
 
--- TestPrimoCompitino2015/src/TrenoMetro.java
+++ TestPrimoCompitino2015/src/TrenoMetro.java
@@ -1,5 +1,48 @@
-
 public class TrenoMetro {
+	
+	private int numeroPasseggeri = 0;
+	private Passeggero[] passeggeri;
+	
 	public TrenoMetro() {
+		
+	}
+	
+	public boolean salitaPasseggero(String destinazione, boolean inPossessoDiBiglietto) {
+		if (numeroPasseggeri >= 50) {
+			System.out.println("Errore, raggiuntacapienza massima treno!");
+			return false;
+		}
+		
+		Passeggero p = new Passeggero(destinazione, inPossessoDiBiglietto);
+		
+		passeggeri[numeroPasseggeri] = p;
+		numeroPasseggeri++;
+		System.out.println("num passeggeri:" + numeroPasseggeri);
+		return true;
+	}
+	
+	public Passeggero[] discesaPasseggeri(String destinazione) {		
+		Passeggero[] passeggeriConDest = new Passeggero[numeroPasseggeri];
+		int numPasseggeriConDest = 0;
+		for (int i = 0; i < numeroPasseggeri; i++) {
+			Passeggero p = passeggeri[i];
+			
+			if (p != null) {
+				if (p.getDestinazione() == destinazione) {
+					if (p.isInPosessoDiBiglietto() == true) {
+						passeggeri[i] = null;
+					} else {
+						passeggeriConDest[numPasseggeriConDest] = p;
+						numPasseggeriConDest++;
+					}
+					numeroPasseggeri--;
+				}
+			}
+		}
+		return passeggeriConDest;
+	}
+
+	public int numeroPasseggeri() {
+		return numeroPasseggeri;
 	}	
 }
