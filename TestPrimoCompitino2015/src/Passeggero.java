61e5d01e4a9a361b71d6846b3f4cc3eb6d8c90e3
Cesare de Cal <contact@cesare.io>
2018-04-05 11:02:51 +0200

    Updated TestMetro class

--- TestPrimoCompitino2015/src/Passeggero.java
+++ TestPrimoCompitino2015/src/Passeggero.java
@@ -1,16 +1,27 @@
+/*
+ * Cesare Gianfilippo Astianatte De Cal
+ * 
+ * Rappresenta	 un	 passeggero	 della	 metropolitana.
+ */
+
 public class Passeggero {
 	private String destinazione;
 	private boolean inPossessoDiBiglietto;
-		
-	public Passeggero(String destinazione) {
-		setAttributes(destinazione, true);
-	}
-
+	
 	public Passeggero(String destinazione, boolean inPossessoDiBiglietto) {
-		setAttributes(destinazione, inPossessoDiBiglietto);
+		creaPasseggero(destinazione, inPossessoDiBiglietto);
 	}
 	
-	private void setAttributes(String destinazione, boolean inPossessoDiBiglietto) {
+	public Passeggero(String destinazione) {
+		creaPasseggero(destinazione, true);
+	}
+	
+	@Override
+	public String toString() {
+		return "Passeggero [destinazione=" + destinazione + ", inPossessoDiBiglietto=" + inPossessoDiBiglietto + "]";
+	}
+	
+	private void creaPasseggero(String destinazione, boolean inPossessoDiBiglietto) {
 		if (destinazione == null) {
 			this.destinazione = "CAPOLINEA";
 		} else {
@@ -18,25 +29,16 @@ public class Passeggero {
 		}
 		this.inPossessoDiBiglietto = inPossessoDiBiglietto;
 	}
-	
-	public String getDestinazione() {
-		return destinazione;
-	}
-
+		
 	public void setDestinazione(String destinazione) {
 		this.destinazione = destinazione;
 	}
 
+	public String getDestinazione() {
+		return destinazione;
+	}
+
 	public boolean isInPosessoDiBiglietto() {
 		return inPossessoDiBiglietto;
-	}
-
-	public void setInPossessoDiBiglietto(boolean inPossessoDiBiglietto) {
-		this.inPossessoDiBiglietto = inPossessoDiBiglietto;
-	}
-
-	@Override
-	public String toString() {
-		return "Passeggero [destinazione=" + destinazione + ", inPossessoDiBiglietto=" + inPossessoDiBiglietto + "]";
 	}
 }
--- TestPrimoCompitino2015/src/TrenoMetro.java
+++ TestPrimoCompitino2015/src/TrenoMetro.java
@@ -1,44 +1,61 @@
+/*
+ * Cesare Gianfilippo Astianatte De Cal
+ * 
+ * Rappresenta	un	vagone	della	metro	della	capienza di	50	passeggeri.
+ */
+
 public class TrenoMetro {
 	
 	private int numeroPasseggeri = 0;
 	private Passeggero[] passeggeri = new Passeggero[50];
 	
 	public TrenoMetro() {
+	}
 		
+	public boolean salitaPasseggero(String destinazione, boolean inPossessoDiBiglietto) {
+		return passeggeroSale(destinazione, inPossessoDiBiglietto);
+	}
+
+	public boolean salitaPasseggero(String destinazione) {
+		return passeggeroSale(destinazione, true);
 	}
 	
-	public boolean salitaPasseggero(String destinazione, boolean inPossessoDiBiglietto) {
+	private boolean passeggeroSale(String destinazione, boolean inPossessoDiBiglietto) {
 		if (numeroPasseggeri >= 50) {
-			System.out.println("Errore, raggiuntacapienza massima treno!");
 			return false;
 		}
 		
 		Passeggero p = new Passeggero(destinazione, inPossessoDiBiglietto);
-		
 		passeggeri[numeroPasseggeri] = p;
-		numeroPasseggeri++;
+		numeroPasseggeri += 1;
 		return true;
 	}
 	
-	public Passeggero[] discesaPasseggeri(String destinazione) {		
-		Passeggero[] passeggeriConDest = new Passeggero[numeroPasseggeri];
-		int numPasseggeriConDest = 0;
-		for (int i = 0; i < numeroPasseggeri; i++) {
+	public Passeggero[] discesaPasseggeri(String destinazione) {
+		Passeggero[] passeggeriRimossi = new Passeggero[50];
+		int numeroPasseggeriRimossi = 0;
+				
+		for (int i = 0; i < numeroPasseggeriRimossi; i++) {
 			Passeggero p = passeggeri[i];
 			
-			if (p != null) {
-				if (p.getDestinazione() == destinazione) {
-					if (p.isInPosessoDiBiglietto() == true) {
-						passeggeri[i] = null;
-					} else {
-						passeggeriConDest[numPasseggeriConDest] = p;
-						numPasseggeriConDest++;
-					}
-					numeroPasseggeri--;
+			if (p.getDestinazione() == destinazione) {
+				passeggeri[i] = null;
+				numeroPasseggeri -= 1;
+
+				if (p.isInPosessoDiBiglietto() == false) {
+					passeggeriRimossi[numeroPasseggeriRimossi] = p;
+					numeroPasseggeriRimossi++;
 				}
 			}
 		}
-		return passeggeriConDest;
+				
+		Passeggero[] passeggeriRimossiCopia = new Passeggero[numeroPasseggeriRimossi];
+	
+		for (int i = 0; i < numeroPasseggeriRimossi; i++) {
+			passeggeriRimossiCopia[i] = passeggeriRimossi[i];
+		}
+					
+		return passeggeriRimossiCopia;
 	}
 
 	public int numeroPasseggeri() {
